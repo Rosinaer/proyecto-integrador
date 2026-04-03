a58b298daafb7c2a893b626b55203073addc8c75
Rosinaer <rosinaer@hotmail.com>
2026-04-03 18:18:01 -0300

    Se integra rutas de cliente y servicio en app.js

--- estetica-backend/app.js
+++ estetica-backend/app.js
@@ -7,9 +7,13 @@ app.use(express.json());
 
 const authRoutes = require("./src/routes/auth.routes");
 const usuarioRoutes = require("./src/routes/usuario.routes");
+const clienteRoutes = require("./src/routes/cliente.routes");
+const servicioRoutes = require("./src/routes/servicio.routes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/cliente", clienteRoutes);
+app.use("/api/servicio", servicioRoutes);
 
 app.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000");
